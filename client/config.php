57e1d086ae53c283b5060ac3f99aac444ca8f8ca
cremesk <ennik@envs.net>
2019-07-25 23:28:08 +0200

    fork to envs.net

--- client/config.php
+++ client/config.php
@@ -1,6 +1,7 @@
 <?php
 	// Location of CSS
-	define("CSS_LOCATION","https://tilde.team/css/hacker.css");
+//	define("CSS_LOCATION","https://envs.net/css/css_style.css");
+	define("CSS_LOCATION","https://tilde.team/css/dracula.css");
 
 	// Host and Port of BBJ instance
 	define("BBJ_HOST","127.0.0.1");
--- client/header.php
+++ client/header.php
@@ -11,8 +11,24 @@
 		word-wrap: break-word;
 		word-break: normal;
 	}
+	a { text-decoration: none; }
+	a:hover { border-bottom: dotted 1px; }
+	.button_back {
+		float: left;
+		margin-top: 0.8em;
+		margin-left: 0.4em;
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+	.clean { margin: 0; padding: 0; }
 	</style>
 </head>
 <body>
+
+	<div class="button_back">
+		<pre class="clean"><strong><a href="https://envs.net">&lt; envs.net</a></strong></pre>
+	</div>
+
 	<div class="container">
 		<h1><?=$title?></h1>
